414042fc2caa78b91d550c235e85be27f9d1eaf1
CandyTale55 <candytale55@gmail.com>
2025-08-25 21:31:48 +0200

    #1 add main.js code as it was tested before moving the openai stuff to the backend.

--- dodgy-dave-predictor/dodgy-dave-cloudflare-worker/src/index.js
+++ dodgy-dave-predictor/dodgy-dave-cloudflare-worker/src/index.js
@@ -6,10 +6,21 @@
  * - Run `npm run deploy` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
- */
+ * 
+ * 
+
+## Install OpenAI in your Worker project
+npm install openai
+
+## Save API key to your Workers environment
+npx wrangler secret put OPENAI_API_KEY
+
+## Deploy the latest Worker changes
+npx wrangler deploy 
+*/
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		return new Response('Hello World!, I see you Girl!');
 	},
 };
